9190669dd0cab02742c5a4484117d3c4282dd4d5
NvChr0n0 <155687210+NvChr0n0@users.noreply.github.com>
2025-03-06 03:19:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+setTimeout(function carregamento(){
+    document.getElementsByClassName("loading")[0].style.display = "none";
+    document.getElementsByClassName("content")[0].style.display = "block";
+},8000);
